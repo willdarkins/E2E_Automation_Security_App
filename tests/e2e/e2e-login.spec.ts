3d4a5d0d89240022c38d2a193def40329c18bea2
Will Darkins <will.darkins@adcellerant.com>
2024-10-09 21:53:36 -0700

    created positive and negative test scnearios for logging in and out of site... also verified logout page

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,15 +1,30 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('login/logout flow', () => {
+test.describe.parallel('login/logout flow', () => {
     //before hook to send to website for each test
     test.beforeEach(async ({page}) => {
         await page.goto('http://zero.webappsecurity.com/')
     })
     //negative scenario
-    test('negative scneario for login', async ({page}) => {
+    test('negative scenario for login', async ({page}) => {
         await page.click('#signin_button')
-        await page.fill('#user_login', 'some username')
-        await page.fill('#user_password', 'some password')
+        await page.fill('#user_login', 'invalid username')
+        await page.fill('#user_password', 'invalid password')
+        await page.click('text=Sign in')
+        const errorMessage = await page.locator('.alert-error')
+        await expect(errorMessage).toContainText('Login and/or password are wrong.')
     })
     //positive scenario + logout
+    test('positive scenario for Login + Logout', async ({page}) => {
+        await page.click('#signin_button')
+        await page.fill('#user_login', 'username')
+        await page.fill('#user_password', 'password')
+        await page.click('text=Sign in')
+        
+        const accountSummaryTab = await page.locator('#account_activity_tab')
+        await expect(accountSummaryTab).toBeVisible
+
+        await page.goto('http://zero.webappsecurity.com/index.html')
+        await expect(page).toHaveURL('http://zero.webappsecurity.com/index.html')
+    })
 })
