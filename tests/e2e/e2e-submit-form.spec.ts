d32cb5d3e5ead409c112cf217aadf395c468b560
Will Darkins <will.darkins@adcellerant.com>
2024-10-14 14:43:11 -0700

    created test to verify clear form button works

--- tests/e2e/e2e-submit-form.spec.ts
+++ tests/e2e/e2e-submit-form.spec.ts
@@ -5,5 +5,24 @@ test.describe('feedback form', () => {
         await page.goto('http://zero.webappsecurity.com/')
         await page.click('#feedback')
     })
-    
+    // Reset feeback form
+    test('reset feedback form', async ({ page }) => {
+        await page.fill('#name', 'some name')
+        await page.fill('#email', 'willdarkins@gmail.com')
+        await page.fill('#subject', 'I am angry')
+        await page.fill('#comment', 'Can someone please help me?')
+        await page.click("input[name='clear']")
+
+        const nameInput = await page.locator('#name')
+        const emailInput = await page.locator('#email')
+        const subjectInput = await page.locator('#subject')
+        const commentInput = await page.locator('#comment')
+
+        await expect(nameInput).toBeEmpty()
+        await expect(emailInput).toBeEmpty()
+        await expect(subjectInput).toBeEmpty()
+        await expect(commentInput).toBeEmpty()
+
+    })
+    // Submit feeback form
 })
