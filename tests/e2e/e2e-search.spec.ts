0391e9e8fa373292d1e4b236511b70b6f379f0e6
Will Darkins <will.darkins@adcellerant.com>
2024-10-14 15:20:24 -0700

    finishing up verfying links populate on search term

--- tests/e2e/e2e-search.spec.ts
+++ tests/e2e/e2e-search.spec.ts
@@ -5,5 +5,8 @@ test.describe('search results', () => {
         await page.goto('http://zero.webappsecurity.com/index.html')
         await page.fill('#searchTerm', 'bank')
         await page.keyboard.press('Enter')
+
+        const numberOfLinks = await page.locator('li > a')
+        await expect(numberOfLinks).toHaveCount(2)
     })
 })
