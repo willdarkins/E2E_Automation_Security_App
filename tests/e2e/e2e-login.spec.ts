fa064195cd626d00114212b9d9c836722eb07e60
Will Darkins <will.darkins@adcellerant.com>
2024-10-14 14:21:10 -0700

    quick fix for SLL Certificate bug

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -16,6 +16,7 @@ test.describe.parallel('login/logout flow', () => {
     })
     //positive scenario + logout
     test('positive scenario for Login + Logout', async ({page}) => {
+        await page.goto('http://zero.webappsecurity.com/index.html')
         await page.click('#signin_button')
         await page.fill('#user_login', 'username')
         await page.fill('#user_password', 'password')
