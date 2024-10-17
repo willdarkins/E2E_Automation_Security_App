f43d2dd99fa4470ebd069787485db7a9538169e2
Will Darkins <will.darkins@adcellerant.com>
2024-10-17 14:28:51 -0700

    defined selectors and constructor for login page method

--- page-objects/LoginPage.ts
+++ page-objects/LoginPage.ts
@@ -1,2 +1,20 @@
 import { expect, Locator, Page } from '@playwright/test'
 
+export class LoginPage {
+    //Define selectors
+    readonly page: Page;
+    readonly usernameInput: Locator;
+    readonly passwordInput: Locator;
+    readonly submitButton: Locator;
+    readonly errorMessage: Locator;
+
+    //Init selectors using constructors
+    constructor(page:Page) {
+        this.page = page;
+        this.usernameInput = page.locator('#user_login')
+        this.passwordInput = page.locator('#user_password')
+        this.submitButton = page.locator('text=Sign in')
+        this.errorMessage = page.locator('.alert-error')
+    }
+    //Define login page methods 
+}
