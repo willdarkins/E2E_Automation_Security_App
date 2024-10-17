1bd76ecdba29dff68b0f3abd7a65257a2d7135be
Will Darkins <will.darkins@adcellerant.com>
2024-10-17 14:08:01 -0700

    created page-objects foler and file to begin refactoring code to POM... Login page to begin

--- tests/e2e/e2e-purchase-foreign-currency.spec.ts
+++ tests/e2e/e2e-purchase-foreign-currency.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('currency exchange', () => {
+test.describe.only('currency exchange', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto('http://zero.webappsecurity.com/index.html')
         await page.click('#signin_button')
@@ -14,6 +14,19 @@ test.describe('currency exchange', () => {
         await expect(header).toContainText('Make payments to your saved payees')
 
         await page.click('text=Purchase Foreign Currency')
-        
+        await page.selectOption('#pc_currency','CNY')
+        await page.waitForSelector('#sp_sell_rate')
+       
+        await page.fill('#pc_amount', '1000')
+
+        await page.click('#pc_inDollars_true')
+        await page.click('#pc_calculate_costs')
+
+        await page.waitForSelector('#pc_conversion_amount')
+
+        await page.click('#purchase_cash')
+        const message = await page.locator('#alert_content')
+        await expect(message).toBeVisible()
+        await expect(message).toContainText('Foreign currency cash was successfully purchased.')
     })
 })
