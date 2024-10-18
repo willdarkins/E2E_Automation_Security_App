f1d7400017339d469ec1053ba82c090a30ec5721
Will Darkins <will.darkins@adcellerant.com>
2024-10-17 20:21:35 -0700

    added POM construct to HomePage and cut down on code

--- page-objects/LoginPage.ts
+++ page-objects/LoginPage.ts
@@ -5,26 +5,20 @@ export class LoginPage {
     readonly page: Page;
     readonly usernameInput: Locator;
     readonly passwordInput: Locator;
-    readonly signInButton: Locator;
     readonly submitButton: Locator;
     readonly errorMessage: Locator;
 
     //Init selectors using constructors
     constructor(page:Page) {
         this.page = page;
-        this.signInButton = page.locator('#signin_button')
         this.usernameInput = page.locator('#user_login')
         this.passwordInput = page.locator('#user_password')
         this.submitButton = page.locator('text=Sign in')
         this.errorMessage = page.locator('.alert-error')
     }
     //Define login page methods 
-    async visit() {
-        await this.page.goto('http://zero.webappsecurity.com/')
-    }
-
+    
     async login(username: string, password: string) {
-        await this.signInButton.click()
         await this.usernameInput.fill(username)
         await this.passwordInput.fill(password)
         await this.submitButton.click()
--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,29 +1,33 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../../page-objects/LoginPage'
+import { HomePage } from '../../page-objects/HomePage'
 
-test.describe.only('login/logout flow', () => {
+test.describe.parallel.only('login/logout flow', () => {
     let loginPage: LoginPage
+    let homePage: HomePage
 
     //before hook to send to website for each test
     test.beforeEach(async ({page}) => {
+        homePage = new HomePage(page)
         loginPage = new LoginPage(page)
-        await loginPage.visit()
+
+        await homePage.visit()
     })
     //negative scenario
     test('negative scenario for login', async ({ page }) => {
-        // await page.click('#signin_button')
+        await homePage.clickOnSignIn()
         await loginPage.login('invalid username', 'invalid password')
         await loginPage.assertErrorMessage()
 
     })
     //positive scenario + logout
     test('positive scenario for Login + Logout', async ({ page }) => {
-        // await page.click('#signin_button')
-        
+        await homePage.clickOnSignIn()
         await loginPage.login('username', 'password')
         
         const accountSummaryTab = await page.locator('#account_activity_tab')
         await expect(accountSummaryTab).toBeVisible
+
         //below... I'm now navigating to the logout page to confirm it exists
         await page.goto('http://zero.webappsecurity.com/index.html')
         await expect(page).toHaveURL('http://zero.webappsecurity.com/index.html')
