fcc467f76b1c0f6c00149bae2895c8013357950a
Will Darkins <will.darkins@adcellerant.com>
2024-10-17 19:55:52 -0700

    cant remember what i changed... just committing

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,26 +1,26 @@
 import { test, expect } from '@playwright/test'
+import { LoginPage } from '../../page-objects/LoginPage'
 
-test.describe.parallel('login/logout flow', () => {
+test.describe.only('login/logout flow', () => {
+    let loginPage: LoginPage
+
     //before hook to send to website for each test
     test.beforeEach(async ({page}) => {
-        await page.goto('http://zero.webappsecurity.com/')
+        loginPage = new LoginPage(page)
+        await loginPage.visit()
     })
     //negative scenario
-    test('negative scenario for login', async ({page}) => {
-        await page.click('#signin_button')
-        await page.fill('#user_login', 'invalid username')
-        await page.fill('#user_password', 'invalid password')
-        await page.click('text=Sign in')
-        const errorMessage = await page.locator('.alert-error')
-        await expect(errorMessage).toContainText('Login and/or password are wrong.')
+    test('negative scenario for login', async ({ page }) => {
+        // await page.click('#signin_button')
+        await loginPage.login('invalid username', 'invalid password')
+        await loginPage.assertErrorMessage()
+
     })
     //positive scenario + logout
-    test('positive scenario for Login + Logout', async ({page}) => {
-        await page.goto('http://zero.webappsecurity.com/index.html')
-        await page.click('#signin_button')
-        await page.fill('#user_login', 'username')
-        await page.fill('#user_password', 'password')
-        await page.click('text=Sign in')
+    test('positive scenario for Login + Logout', async ({ page }) => {
+        // await page.click('#signin_button')
+        
+        await loginPage.login('username', 'password')
         
         const accountSummaryTab = await page.locator('#account_activity_tab')
         await expect(accountSummaryTab).toBeVisible
