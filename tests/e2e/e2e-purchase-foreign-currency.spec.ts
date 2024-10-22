401253f376b003d049c883292c9236fa499cdb1a
Will Darkins <will.darkins@adcellerant.com>
2024-10-22 07:47:32 -0700

    started refactoring currency exchange test to be E2E

--- tests/e2e/e2e-purchase-foreign-currency.spec.ts
+++ tests/e2e/e2e-purchase-foreign-currency.spec.ts
@@ -1,12 +1,22 @@
 import { test, expect } from '@playwright/test'
+import { LoginPage } from '../../page-objects/LoginPage'
+import { HomePage } from '../../page-objects/HomePage'
 
 test.describe('currency exchange', () => {
+    let loginPage: LoginPage
+    let homePage: HomePage
+
     test.beforeEach(async ({ page }) => {
-        await page.goto('http://zero.webappsecurity.com/index.html')
-        await page.click('#signin_button')
-        await page.fill('#user_login', 'username')
-        await page.fill('#user_password', 'password')
-        await page.click('text=Sign in')
+        homePage = new HomePage(page)
+        loginPage = new LoginPage(page) 
+        
+        homePage.visit()
+        loginPage.login('username', 'password')
+        // await page.goto('http://zero.webappsecurity.com/index.html')
+        // await page.click('#signin_button')
+        // await page.fill('#user_login', 'username')
+        // await page.fill('#user_password', 'password')
+        // await page.click('text=Sign in')
     })
     test('convert currency', async ({ page }) => {
         await page.goto('http://zero.webappsecurity.com/bank/pay-bills.html')
