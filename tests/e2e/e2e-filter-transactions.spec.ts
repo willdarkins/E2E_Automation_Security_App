2e54409e92c254a8602562b02278d3e12bbf740e
Will Darkins <will.darkins@adcellerant.com>
2024-10-26 11:36:07 -0700

    refactored homepage and login functions for funds transfer, pay bills and filter transactions

--- tests/e2e/e2e-filter-transactions.spec.ts
+++ tests/e2e/e2e-filter-transactions.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 import { LoginPage } from '../../page-objects/LoginPage'
 import { HomePage } from '../../page-objects/HomePage'
 
-test.describe.only('filter transactions', () => {
+test.describe('filter transactions', () => {
     let homePage: HomePage
     let loginPage: LoginPage
     
--- tests/e2e/e2e-transfer-funds.spec.ts
+++ tests/e2e/e2e-transfer-funds.spec.ts
@@ -1,12 +1,18 @@
 import { test, expect } from '@playwright/test'
+import { LoginPage } from '../../page-objects/LoginPage'
+import { HomePage } from '../../page-objects/HomePage'
 
-test.describe('transfer funds and make payments', () => {
+test.describe.only('transfer funds and make payments', () => {
+    let homePage: HomePage
+    let loginPage: LoginPage
+    
     test.beforeEach(async ({ page }) => {
-        await page.goto('http://zero.webappsecurity.com/index.html')
-        await page.click('#signin_button')
-        await page.fill('#user_login', 'username')
-        await page.fill('#user_password', 'password')
-        await page.click('text=Sign in')
+        homePage = new HomePage(page)
+        loginPage = new LoginPage(page) 
+        
+       await homePage.visit()
+       await homePage.clickOnSignIn()
+       await loginPage.login('username', 'password')
     })
     test('transfer funds', async ({ page }) => {
         await page.goto('http://zero.webappsecurity.com/bank/transfer-funds.html')
