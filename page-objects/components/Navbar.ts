b516c18baa3ac450a4c760276c6e6daec9cd4691
Will Darkins <will.darkins@adcellerant.com>
2024-10-26 11:51:38 -0700

    wrote out function for switching between tabs in navbard component

--- page-objects/components/Navbar.ts
+++ page-objects/components/Navbar.ts
@@ -11,11 +11,36 @@ export class Navbar {
 
     constructor(page:Page) {
         this.page = page
-        this.accountSummary = page.locator('account_summary_tab')
-        this.accountActivity = page.locator('account_activity_tab')
-        this.transferFunds = page.locator('transfer_funds_tab')
-        this.payBills = page.locator('pay_bills_tab')
-        this.myMoneyMap = page.locator('money_map_tab')
-        this.onlineStatements = page.locator('online_statements_tab')
+        this.accountSummary = page.locator('#account_summary_tab')
+        this.accountActivity = page.locator('#account_activity_tab')
+        this.transferFunds = page.locator('#transfer_funds_tab')
+        this.payBills = page.locator('#pay_bills_tab')
+        this.myMoneyMap = page.locator('#money_map_tab')
+        this.onlineStatements = page.locator('#online_statements_tab')
+    }
+    //Going to use a Javascript switch function to have the ability to switch between tabs
+    async clickOnTab(tabName) {
+        switch(tabName) {
+            case 'Account Summary':
+                await this.accountSummary.click()
+                break
+            case 'Account Activity':
+                await this.accountActivity.click()
+                break
+            case 'Transfer Funds':
+                    await this.transferFunds.click()
+                    break
+            case 'Pay Bills':
+                    await this.payBills.click()
+                    break
+            case 'My Money Map':
+                    await this.myMoneyMap.click()
+                    break
+            case 'Online Statements':
+                    await this.onlineStatements.click()
+                    break
+            default:
+                throw new Error('This tab does not exist...')      
+        }
     }
 }
