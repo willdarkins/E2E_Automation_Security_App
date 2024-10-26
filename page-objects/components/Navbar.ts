c9e788323578832e84ef66d454d8aff93acc6940
Will Darkins <will.darkins@adcellerant.com>
2024-10-26 11:45:13 -0700

    created constructor for navbar compoenents

--- page-objects/components/Navbar.ts
+++ page-objects/components/Navbar.ts
@@ -8,4 +8,14 @@ export class Navbar {
     readonly payBills: Locator
     readonly myMoneyMap: Locator
     readonly onlineStatements: Locator
+
+    constructor(page:Page) {
+        this.page = page
+        this.accountSummary = page.locator('account_summary_tab')
+        this.accountActivity = page.locator('account_activity_tab')
+        this.transferFunds = page.locator('transfer_funds_tab')
+        this.payBills = page.locator('pay_bills_tab')
+        this.myMoneyMap = page.locator('money_map_tab')
+        this.onlineStatements = page.locator('online_statements_tab')
+    }
 }
