34af8cd0dcc5f3c536e212df06a59854bd7389dd
Will Darkins <will.darkins@adcellerant.com>
2024-10-29 10:32:05 -0700

    created single element visual test

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -4,7 +4,13 @@ import { test, expect } from '@playwright/test'
 ///First test will fail because it's expecting something there - but will pass after a snapshot is taken, then compared
 test.describe('visual regression testing', () => {
     test('full page snapshot', async ({ page }) => {
-        await page.goto('http://zero.webappsecurity.com/index.html')
+        await page.goto('https://www.example.com')
         expect(await page.screenshot()).toMatchSnapshot('homepage.png')
+    })
+
+    test('single element snapshot', async ({ page }) => {
+        await page.goto('https://www.example.com')
+        const pageElement = await page.$('h1')
+        expect(await pageElement?.screenshot()).toMatchSnapshot('page-title.png')
     })
 })
