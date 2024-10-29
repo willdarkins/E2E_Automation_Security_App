9d25f172ec3d0317f3e562374069658b6f051a5b
Will Darkins <will.darkins@adcellerant.com>
2024-10-29 11:56:10 -0700

    created new test for a GET request of singular user... asserting names, ids, email and successful connection

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, request } from '@playwright/test'
 
 test.describe.parallel('api testing', () => {
     const baseUrl = 'https://reqres.in/api' //creating variable for baseurl where api is being called
@@ -11,5 +11,19 @@ test.describe.parallel('api testing', () => {
     test('simple api test - assert invalid endpoint', async ({ request }) => {
         const response = await request.get(`${baseUrl}/users/poop_pants`)
         expect(response.status()).toBe(404)
+    })
+
+    test.only('GET request - get user details', async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/1`)
+        const responseBody = JSON.parse(await response.text()) //variable that parses JSON data into readable text on our end
+
+        expect(response.status()).toBe(200) //asserts the call to be successful
+        expect(responseBody.data.id).toBe(1) //asserts the id of the endpoint user we're hitting is "1"... look at the response variable to make the connection
+        expect(responseBody.data.first_name).toBe('George') //using the function 'toBe' because we're asserting values in API, not actual text
+        expect(responseBody.data.last_name).toBe('Bluth')
+        expect(responseBody.data.email).toBeTruthy() //verifying there is any value at all for the email address of the user being tapped in question
+
+        // console.log(responseBody)
+        //running the console.log to being in order to see the data and get more info about it
     })
 })
