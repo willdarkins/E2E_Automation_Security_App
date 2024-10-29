eea86c4d2183e439da3b195db9c53a28de65e9dc
Will Darkins <will.darkins@adcellerant.com>
2024-10-29 11:03:43 -0700

    created config file for api playwright testing and created new script in package.json

--- tests/visual/login-visual.spec.ts
+++ tests/visual/login-visual.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test'
 import { HomePage } from '../../page-objects/HomePage'
 import { LoginPage } from '../../page-objects/LoginPage'
 
-test.describe.only('login page visual tests', () => {
+test.describe('login page visual tests', () => {
     let homepage: HomePage
     let loginpage: LoginPage
 
