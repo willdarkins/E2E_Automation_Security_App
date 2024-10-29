19441214c947a46e371fe26d304a08aa0b56fdad
Will Darkins <will.darkins@adcellerant.com>
2024-10-29 10:57:06 -0700

    updated visual config file to be browser specific on updating screenshots per browser

--- tests/visual/login-visual.spec.ts
+++ tests/visual/login-visual.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test'
 import { HomePage } from '../../page-objects/HomePage'
 import { LoginPage } from '../../page-objects/LoginPage'
 
-test.describe('login page visual tests', () => {
+test.describe.only('login page visual tests', () => {
     let homepage: HomePage
     let loginpage: LoginPage
 
@@ -10,6 +10,16 @@ test.describe('login page visual tests', () => {
         homepage = new HomePage(page)
         loginpage = new LoginPage(page)
 
-        
+        await homepage.visit()
+        await homepage.clickOnSignIn()
+    })
+
+    test('login form', async({ page }) => {
+        await loginpage.snapshotLoginForm()
+    })
+
+    test('login error message', async({ page }) => {
+        await loginpage.login('failusername', 'invalidpassword')
+        await loginpage.snapshotErrorMessage()
     })
 })
