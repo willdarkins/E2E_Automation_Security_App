9560a282f190bc30578d1bd6d5534a476847780b
Will Darkins <will.darkins@adcellerant.com>
2024-10-31 14:31:58 -0700

    created first test case that has testInfo tag on it

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test'
 
+//Below - You're passiong in 'testInfo' in order to get more information about tests run below
 test.describe.only('Tips & Tricks seciton', () => {
-    test('testInfo object', async () => {
-        
+    test('testInfo object', async ({ page }, testInfo) => {
+        await page.goto('https://www.example.com')
+        console.log(testInfo)
     })
 })
