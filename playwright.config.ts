5081d7064ecfd5f526811338b2328c815dc0af1f
Will Darkins <will.darkins@adcellerant.com>
2024-10-31 14:12:49 -0700

    created tips and tricks folder to begin learning about testInfo objects

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ const config: PlaywrightTestConfig = {
     timeout: 60000, //miliseconds you want run for a playwright test to finish
     retries: 0, //How many times you want playwright to rerun failed tests
     //'use' is for specific settings you want to apply to the environment
+    testDir: 'tests/tips',
     use: {
         viewport: {width: 1280, height: 720}, //what dimension the screen should be in
         actionTimeout: 15000, //setting the maximum time allowed for an action to complete before it is considered a failure
---
+++ tests/tips/tips.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test'
+
+test.describe.only('Tips & Tricks seciton', () => {
+    test('testInfo object', async () => {
+        
+    })
+})
